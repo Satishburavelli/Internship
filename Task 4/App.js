ff8e378a9e6483a7b374ae7e7b92f5d688f44ed1
Satishburavelli <138653813+Satishburavelli@users.noreply.github.com>
2024-06-15 10:20:54 +0530

    Add files via upload

---
+++ Task 4/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoList from './components/Todo.js'; 
+function App() {
+  return (
+    <div className="App container my-3 mx-8 "   style={{ backgroundColor:'#33FFF0'}}>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
+  
